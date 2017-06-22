4c86f9c61b7cd5cb7551bdf1a21302e81aaa078d
Denise Mauldin <dmauldin@altius.org>
2017-06-22 12:25:36 -0700

    Removing unused dependencies. Updated test message

--- test/timeline-test.js
+++ test/timeline-test.js
@@ -2,7 +2,7 @@ var tape = require("tape"),
 	d3 = Object.assign({}, require("../"), require("d3")),
     timeline = require("../");
 
-tape("timeline() returns the answer to the ultimate question of life, the universe, and everything.", function(test) {
+tape("timeline() returns the correct version", function(test) {
   test.equal(timeline.timeline().version(), '1.0.0');
   test.end();
 });
