5d9ed62860680f9494e14cbd5a6dfc42842c31b2
Horacio Gonzalez <horacio.gonzalez@gmail.com>
2018-04-27 17:20:28 +0200

    Using getAttibute and setAttribute instead of attr

--- dist/d3-timelines.js
+++ dist/d3-timelines.js
@@ -582,8 +582,8 @@
 							// set height based off of item height
 							height = gSize.height + gSize.top - gParentSize.top;
 							// set bounding rectangle height
-							d3Selection.select(gParent).node().attr("height", height);
-							//select(view).node().attr("height", height);
+							d3Selection.select(gParent).node().setAttribute("height", height);
+							//select(view).node().setAttribute("height", height);
 						} else {
 							throw "height of the timeline is not set";
 						}
@@ -591,8 +591,8 @@
 						if (!height) {
 							height = gParentSize.height;
 						} else {
-							gParentItem.node().attr("height", height);
-							//view.node().attr("height", height);
+							gParentItem.node().setAttribute("height", height);
+							//view.node().setAttribute("height", height);
 						}
 					}
 				}
@@ -600,7 +600,7 @@
 				function setWidth() {
 					if (!width && !gParentSize.width) {
 						try {
-							width = gParentItem.node().attr("width");
+							width = gParentItem.node().getAttribute("width");
 							if (!width) {
 								throw "width of the timeline is not set. As of Firefox 27, timeline().with(x) needs to be explicitly set in order to render";
 							}
